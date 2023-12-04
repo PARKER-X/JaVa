9d69f15d5909b601a511307db9312defa2c87d83
Harsh <harshrawat096@gmail.com>
2023-12-04 21:42:04 +0530

    Variables!

---
+++ Variable.java
@@ -0,0 +1,15 @@
+class Variable{
+    public static void main(String[] args){
+        int num1 = 1;
+        float num2 = 1.2f;
+        char Name = 'I';
+        byte a = 2;
+        short b = 5;
+        long l = 11111;
+        boolean v = true;
+        
+
+        System.out.println(num1 + " " + num2 + " " + Name + " " + a + " " + b + " " + l + " " + v);
+
+    }
+}
