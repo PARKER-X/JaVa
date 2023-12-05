a1039312a099f48dd2ac46051fbfd8ac2460f821
Harsh <harshrawat096@gmail.com>
2023-12-05 21:11:07 +0530

    String

--- Variable.java
+++ Variable.java
@@ -1,13 +1,13 @@
 class Variable{
     public static void main(String[] args){
         int num1 = 1;
-        float num2 = 1.2f;
+        float num2 = 1.2F;
         char Name = 'I';
         byte a = 2;
         short b = 5;
         long l = 11111;
         boolean v = true;
-        
+        String name = "IronMan";
 
         System.out.println(num1 + " " + num2 + " " + Name + " " + a + " " + b + " " + l + " " + v);
 
