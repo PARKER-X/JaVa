cfbe73539dc07c1ceb3499bc4fed320196915f00
Harsh <harshrawat096@gmail.com>
2023-12-15 11:18:21 +0530

    comit change

---
+++ string.java
@@ -0,0 +1,9 @@
+public class string{
+    public static void main(String[] args){
+        String name1 = "IronMan";
+        String name2 = "Thor";
+        String name3 = name1 + " and " + name2;
+        System.out.println(name3);
+
+    }
+}
