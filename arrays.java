5d78182380a998d3a1289e5b488bf2e1e710ffcd
Harsh <harshrawat096@gmail.com>
2023-12-20 19:56:38 +0530

    Casting

--- arrays.java
+++ arrays.java
@@ -4,14 +4,14 @@ public class arrays {
     public static void main(String[] args) {
         // Arrays
         int[] marks = new int[3];
-        // marks[0] = 99;
+        marks[0] = 99;
         marks[1] = 98;
         marks[2] = 97;
 
         System.out.println(marks[0]);
         System.out.println(marks.length);
 
-        int[] marks_s = {99,98,100}
+        int[] marks_s = {99,98,100};
 
         // Sort
         Arrays.sort(marks);
--- casting.java
+++ casting.java
@@ -1,5 +1,15 @@
 public class casting {
     public static void main(String[] args) {
-        
+        // Implicit same type
+        double prices = 100.0;
+        double finalprices = 10 + prices;
+
+        System.out.println(finalprices);
+
+        // Explict if differ types
+        int price = 10;
+        // int final_price = 10+1.8;
+        int final_price = 10 + (int)1.8;
+        System.out.println(final_price);
     }
 }
