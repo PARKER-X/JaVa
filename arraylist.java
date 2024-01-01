805d0773c0af246fc30c152082fa382dd5145a9f
Harsh <harshrawat096@gmail.com>
2024-01-01 21:20:50 +0530

    Arraylist

---
+++ arraylist.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class arraylist {
+    public static void main(String[] args){
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Audi");
+        cars.add("Lamborghini");
+        cars.add("Mercedes");
+        System.out.println(cars);
+    }
+}
